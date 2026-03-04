97ecf85b3793d5290f8e7533a02373db0cfa6479
淅沥沥丶下雨 <43953295+ljxun@users.noreply.github.com>
2026-03-04 13:35:04 +0800

    refactor(ui): 优化参数视图组件结构并更新依赖
    - 将Card组件从ExpansionTileItem外部移至内部作为子组件
    - 为参数组添加内边距以改善视觉效果
    - 更新flutter_form_builder依赖版本从9.1.0到10.3.0+2
    - 修改服务器视图中的选择按钮文本为根路径选择
    - 移除代码编辑器的主题

--- lib/views/server/server_view.dart
+++ lib/views/server/server_view.dart
@@ -121,7 +121,7 @@ class ServerView extends StatelessWidget {
                   }
                   controller.updateRootPathServer(selectedDirectory);
                 },
-                child: Text(I18n.select.tr),
+                child: Text(I18n.select_root_path_server.tr),
               ),
             ],
           ),
@@ -192,7 +192,6 @@ class ServerView extends StatelessWidget {
         styleOptions: EditorModelStyleOptions(
           heightOfContainer: maxHeight,
           editorColor: theme.colorScheme.surface.withOpacity(0.5),
-          theme: theme.brightness == Brightness.dark ? monokaiSublimeTheme : atomOneLightTheme,
         ),
       );
       return CodeEditor(
