c1e39e3f6da8b75ab89aba8fde546644455071b0
xll <591692130@qq.com>
2026-04-07 21:26:01 +0800

    fix(home): 统一网格子项宽高比
    - 移除移动端和桌面端不同的宽高比配置
    - 将所有设备的childAspectRatio统一设置为1.7
    - 解决响应式布局中的显示不一致问题

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -82,7 +82,7 @@ class HomeView extends GetView<HomeController> {
                     // 响应式布局：手机端使用小边距，桌面端使用大边距
                     final horizontalPadding = PlatformUtils.isMobile ? 48.0 : 100.0;
                     final maxCrossAxisExtent = PlatformUtils.isMobile ? 400.0 : 350.0;
-                    final childAspectRatio = PlatformUtils.isMobile ? 2.0 : 1.7;
+                    final childAspectRatio = 1.7;
                     
                     return GridView.builder(
                       padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
